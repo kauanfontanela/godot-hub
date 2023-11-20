39ae4c229d28782067b411a0f4a15ba52ed23c25
kauanfontanela <kauanfontanela09@gmail.com>
2023-11-20 00:01:11 -0300

    header da navbar & estilo acrílico

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -4,46 +4,57 @@ import { Outlet, Link, NavLink } from "react-router-dom";
 import InstallsIcon from "../assets/icons/InstallsIcon.svg";
 import ProjectsIcon from "../assets/icons/ProjectsIcon.svg";
 import CommunityIcon from "../assets/icons/CommunityIcon.svg";
+import ConfigIcon from "../assets/icons/ConfigIcon.svg";
+import ProfilePictureIcon from "../assets/icons/ProfilePictureIcon.svg";
+
+const sidebarItems = [
+  { icon: ProjectsIcon, text: "Projetos" },
+  { icon: InstallsIcon, text: "Versões" },
+  { icon: CommunityIcon, text: "Comunidade" },
+];
 
 const SidebarNavbar = () => {
-  const liClass = "bg-transparent rounded-lg my-1 p-2 flex items-center hover:font-semibold"
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
 
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
+  // const toggleSidebar = () => {
+  //   setIsOpen(!isOpen);
+  // };
 
   return (
     <>
       <div class="flex h-screen bg-gray-200">
-        {/* Sidebar */}
-        <aside class="w-64 bg-gray-800 p-4 text-white">
-          {/* Sidebar Content Goes Here */}
+        <aside className="w-64 bg-gray-800 p-4 text-white">
+          <div className="flex mt-5">
+            {/* <button className="me-auto blur-[0.8px] hover:blur-none"> */}
+            <button className="me-auto hover:drop-shadow-[0px_4px_12px_rgba(255,255,255,.35)] transition-all">
+              <img src={ProfilePictureIcon} className="drop-shadow-[]" alt="" width="42px" />
+            </button>
+
+            <button className="ms-auto opacity-70 hover:opacity-100">
+              <img src={ConfigIcon} alt="" width="26px" />
+            </button>
+          </div>
+
+          <hr className="border-gray-700 border-1 mt-7 mb-5" />
+
           <ul>
-            <li class={liClass}>
-              <img src={ProjectsIcon} alt="" class="w-4 h-4 mr-4" />
-              <p>Projetos</p>
-            </li>
-            <li class={liClass}>
-              <img src={InstallsIcon} alt="" class="w-4 h-4 mr-4" />
-              <p class="">Versões</p>
-            </li>
-            <li class={liClass}>
-              <img src={CommunityIcon} alt="" class="w-4 h-4 mr-4" />
-              <p>Comunidade</p>
-            </li>
-            {/* <li class={liClass}>
-              <p>Sobre</p>
-            </li> */}
+            {sidebarItems.map((item, index) => (
+              <li className="bg-transparent rounded-lg flex items-center" key={index}>
+                <button className="flex items-centerfocus:outline-none hover:font-semibold opacity-70 hover:opacity-100 w-full h-full blur-[0.7px] hover:blur-none transition-all ps-1 py-3">
+                  <img src={item.icon} alt="" className="mr-4" width="22px" />
+                  <p>{item.text}</p>
+                </button>
+              </li>
+            ))}
           </ul>
-        </aside>
+        </aside >
 
         {/* Main Content */}
-        <div class="flex-1 flex flex-col overflow-hidden">
+        < div class="flex-1 flex flex-col overflow-hidden" >
           {/* Page Content Goes Here */}
-          <Outlet />
-        </div>
-      </div>
+          < Outlet />
+        </div >
+      </div >
     </>
   );
 };
