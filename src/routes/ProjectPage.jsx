d700ff920106f3aa384efe73ae19cb282df70139
LucasAAnjos <luquinhacolorado@hotmail.com>
2023-11-25 16:19:05 -0300

    Created ProjectShortcut and added to ProjectPage

--- src/routes/ProjectPage.jsx
+++ src/routes/ProjectPage.jsx
@@ -1,18 +1,26 @@
-import {Link} from 'react-router-dom'
+import ProjectShortcut from './utils/ProjectShortcut';
 
-export default function ProjectPage() {
-    return (
-        <div class="p-8">
-            {/* {header} */}
-            <div className="p-2">
-                <h2 className="text-2xl font-semibold text-white-700">ProjectTitle</h2>
-                <Link to="/produtos" className="text-sm text-gray-400 hover:text-red-700">
-                Path
-                </Link>
-            </div>
-            git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
-        </div>
-    
-    )
+function ProjectPage() {
+  const projects = [
+    { title: 'Projeto 1', version: '1.0.0', path: '/projeto-1' },
+    { title: 'Projeto 2', version: '2.1.0', path: '/projeto-2' },
+    { title: 'Projeto 3', version: '3.5.2', path: '/projeto-3' },
+  ];
+
+  return (
+    <div className="ProjectList">
+      {projects.map((project, index) => (
+        <ProjectShortcut
+          key={index}
+          projectTitle={project.title}
+          projectVersion={project.version}
+          projectPath={project.path}
+        />
+      ))}
+    </div>
+  );
 }
+
+export default ProjectPage;
+git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
---
+++ src/routes/utils/ProjectShortcut.jsx
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom';
+import { Folder } from 'feather-icons-react';
+
+export default function ProjectShortcut({
+  projectTitle,
+  projectVersion,
+  projectPath,
+}) {
+  return (
+    <div class="p-1">
+      {/* {header} */}
+      <div className="flex items-center justify-start w-full h-18 rounded-lg bg-gray-700 px-4">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6a/Godot_icon.svg"
+          alt="Icon"
+          className="w-16 h-16 mr-2 rounded-full"
+        />
+        <div className="flex flex-col">
+          <div className="justify-between">
+            <h2 className="text-xl font-semibold text-white-700">
+              {projectTitle}
+              <span className="text-xs text-gray-400 ml-2">
+                {projectVersion}
+              </span>
+            </h2>
+          </div>
+          <div className="flex items-center">
+            <Folder className="w-3 h-3 mr-2 text-white" />
+            <Link
+              to="/produtos"
+              className="text-sm text-gray-400 hover:text-red-700"
+            >
+              {projectPath}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
