0d6e05f1bcd96491c3c8667bd94f96d4bc747ece
LucasAAnjos <luquinhacolorado@hotmail.com>
2023-11-27 21:45:03 -0300

    Updated VersionPage and created VersionShortcut

--- src/routes/VersionPage.jsx
+++ src/routes/VersionPage.jsx
@@ -1,17 +1,100 @@
-import {Link} from 'react-router-dom'
+import VersionShortcut from './utils/VersionShortcut';
+import React, { useState } from 'react';
 
-export default function ProjectPage() {
-    return (
-        <div class="p-8">
-            {/* {header} */}
-            <div className="p-6">
-                <h2 className="mb-5 text-3xl font-semibold text-white-700">Versões #</h2>
-                <Link to="/produtos" className="text-sm text-teal-600 hover:text-red-700">
-                Voltar
-                </Link>
-            </div>
+function VersionPage() {
+  const versions = [
+    {
+      title: 'B',
+      version: '5.0.0',
+      path: '/projeto-1',
+      icon: '',
+      available: true,
+    },
+    {
+      title: 'Arojeto 2',
+      version: '2.1.0',
+      path: '/projeto-2',
+      icon: '',
+      available: false,
+    },
+    {
+      title: 'Crojeto 3',
+      version: '3.5.2',
+      path: '/projeto-3',
+      icon: '',
+      available: true,
+    },
+  ];
 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortBy, setSortBy] = useState('Nome');
+
+  const filteredVersions = versions.filter((project) =>
+    project.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const sortVersions = (sortBy) => {
+    const sorted = [...filteredVersions].sort((a, b) => {
+      if (sortBy === 'Nome') {
+        return a.title.localeCompare(b.title, 'en', { sensitivity: 'base' });
+      } else if (sortBy === 'Versão') {
+        return b.version.localeCompare(a.version);
+      }
+      return 0;
+    });
+    return sorted;
+  };
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSortChange = (e) => {
+    setSortBy(e.target.value);
+  };
+
+  const sortedversions = sortVersions(sortBy);
+
+  return (
+    <div className="bg-gray-400 min-h-screen">
+      <div className="text-3xl p-4 flex justify-between">
+        <p>Versões</p>
+        <button className="bg-gray-700 text-gray-300 text-sm px-4 py-2 rounded">
+          {' '}
+          Localizar Versão{' '}
+        </button>
+      </div>
+      <div className="ml-5 mr-5 rounded-lg flex justify-between items-center">
+        <div className="search-header flex-grow">
+          <input
+            className="bg-gray-700 p-2 rounded-lg text-gray-300 w-full"
+            type="text"
+            placeholder="Pesquisar versão..."
+            value={searchTerm}
+            onChange={handleSearch}
+          />
         </div>
-    
-    )
+        <div className="filter-section ml-4">
+          <label htmlFor="sort-by">Ordenar por:</label>
+          <select id="sort-by" value={sortBy} onChange={handleSortChange}>
+            <option value="Nome">Nome</option>
+            <option value="Versão">Versão</option>
+          </select>
+        </div>
+      </div>
+      <div className="ProjectList p-4">
+        {sortedversions.map((project, index) => (
+          <VersionShortcut
+            key={index}
+            versionTitle={project.title}
+            versionRelease={project.version}
+            versionPath={project.path}
+            versionAvailable={project.available}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
+
+export default VersionPage;
---
+++ src/routes/utils/VersionShortcut.jsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+// import { Folder } from 'feather-icons-react';
+
+export default function versionShortcut({
+  versionTitle,
+  versionRelease,
+  versionPath,
+  versionType,
+  versionAvailable,
+}) {
+  return (
+    <div class="p-1">
+      {/* {header} */}
+      <div className="flex items-center justify-start w-full h-20 rounded-lg bg-gray-700 px-4">
+        <div className="flex flex-col w-full">
+          <div className="flex justify-between items-center">
+            <h2 className="text-xl font-semibold text-white-700">
+              {versionTitle}
+            </h2>
+          </div>
+          <div className="flex items-center">
+            {/* <Folder className="w-3 h-3 mr-2 text-white" /> */}
+            <p className="text-sm"> bostamijocu de novembro</p>
+          </div>
+        </div>
+        <div className="flex flex-row">
+          {versionAvailable ? (
+            <button className="mr-1 bg-green-700 text-gray-200 text-sm px-5 py-2 rounded transition-all hover:bg-green-600 hover:text-gray-200 hover:shadow-lg">
+              Abrir
+            </button>
+          ) : (
+            <button className="mr-1 bg-gray-200 text-black text-sm px-5 py-2 rounded transition-all hover:bg-gray-400 hover:text-white hover:shadow-lg">
+              Download
+            </button>
+          )}
+          <button className="mr-1 bg-gray-200 text-black text-sm px-5 py-2 rounded transition-all hover:bg-gray-400 hover:text-white hover:shadow-lg">
+            News
+          </button>
+          <button className="bg-gray-200 text-black text-sm px-5 py-2 rounded transition-all hover:bg-gray-400 hover:text-white hover:shadow-lg">
+            Changelog
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
