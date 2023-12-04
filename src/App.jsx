4a1fad25762cac828f46eaf0a177ca5840b49203
Tainara da Rosa <85135620+tamiriel@users.noreply.github.com>
2023-12-04 23:37:54 +0000

    +

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import RootLayout from "./layouts/RootLayout.jsx";
 import ProjectPage from "./routes/ProjectPage.jsx"
 import VersionPage from "./routes/VersionPage.jsx"
 import CommunityPage from "./routes/CommunityPage.jsx"
+import GlobalStyle from "./styles/global";
+import { AuthProvider } from "./contexts/auth";
+import RoutesApp from "./routes";
+import React from "react";
 
 const router = createBrowserRouter([
   {
